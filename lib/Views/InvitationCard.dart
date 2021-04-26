cbc234b39518c22c4645e4c89406641d8b3ce30e
Jainam Shah <shahjainam1611@gmail.com>
2021-04-26 13:10:05 +0530

    Added Invitation card QR code generator, sharing

--- lib/Views/InvitationCard.dart
+++ lib/Views/InvitationCard.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Models/Event.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
 
 class InvitationCard extends StatefulWidget {
   final String guestDocumentId;
@@ -25,7 +31,6 @@ class _InvitationCardState extends State<InvitationCard> {
   TimeOfDay selectedStartTime = TimeOfDay.now();
   bool isEndTimeUpdated = false;
   TimeOfDay selectedEndTime = TimeOfDay.now();
-  String eventName;
   final event =
       new Event(null, null, null, null, null, null, null, null, null, null);
   _InvitationCardState(this.guestDocumentId, this.eventDocumentId, this.user);
@@ -52,6 +57,7 @@ class _InvitationCardState extends State<InvitationCard> {
     TextEditingController eventLocation = new TextEditingController();
     TextEditingController guestName = new TextEditingController();
     TextEditingController invitationType = new TextEditingController();
+    ScreenshotController _screenshotController = new ScreenshotController();
     eventDocumentReference.get().then((eventDatasnapshot) {
       if (eventDatasnapshot.exists) {
         print('event Found!');
@@ -100,6 +106,20 @@ class _InvitationCardState extends State<InvitationCard> {
         guestName.text = guestDatasnapshot.data['guestName'];
       }
     });
+    Map<String, String> data = <String, String>{
+      "eventId": eventDocumentId,
+      "guestId": guestDocumentId,
+    };
+
+    void _takeScreenshot() async {
+      final filePath = (await getExternalStorageDirectory()).path;
+      final imageFile = await _screenshotController.capture();
+      File card = new File('$filePath/screenshot.png');
+      card.writeAsBytes(imageFile);
+      Share.shareFiles([card.path]);
+      Navigator.of(context).pop();
+    }
+
     return Scaffold(
         appBar: new AppBar(
           title: new Text('Invitation Card'),
@@ -112,117 +132,143 @@ class _InvitationCardState extends State<InvitationCard> {
             child: Column(children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child:
-                    Text('You Are Invited!', style: TextStyle(fontSize: 30.0)),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  readOnly: true,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'Guest Name:',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0)),
-                  ),
-                  controller: guestName,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: 'Event Name:',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(360.0)),
-                  ),
-                  controller: eventName,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: 'Event Type',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0)),
-                  ),
-                  controller: eventType,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new Flexible(
-                      child: TextFormField(
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          labelText: 'Event Start Date',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20.0)),
-                        ),
-                        controller: eventStartDate,
+                child: Screenshot(
+                  controller: _screenshotController,
+                  child: Card(
+                    child: Column(children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('You Are Invited!',
+                            style: TextStyle(fontSize: 30.0)),
                       ),
-                    ),
-                    new Flexible(
-                      child: TextFormField(
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          labelText: 'Event start time',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20.0)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Guest Name:',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
+                          controller: guestName,
                         ),
-                        controller: eventStartTime,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    new Flexible(
-                      child: TextFormField(
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          labelText: 'Event End Date',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20.0)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            labelText: 'Event Name:',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(360.0)),
+                          ),
+                          controller: eventName,
                         ),
-                        controller: eventEndDate,
                       ),
-                    ),
-                    new Flexible(
-                      child: TextFormField(
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          labelText: 'Event End time',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20.0)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            labelText: 'Event Type',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
+                          controller: eventType,
                         ),
-                        controller: eventEndTime,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: 'Event Place',
-                    hintText: '440 Banquets',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            new Flexible(
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Event Start Date',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                ),
+                                controller: eventStartDate,
+                              ),
+                            ),
+                            new Flexible(
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Event start time',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                ),
+                                controller: eventStartTime,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: <Widget>[
+                            new Flexible(
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Event End Date',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                ),
+                                controller: eventEndDate,
+                              ),
+                            ),
+                            new Flexible(
+                              child: TextFormField(
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Event End time',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                ),
+                                controller: eventEndTime,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            labelText: 'Event Place',
+                            hintText: '440 Banquets',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                          ),
+                          controller: eventLocation,
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: QrImage(
+                            data: data.toString(),
+                            size: 150.0,
+                          )),
+                    ]),
                   ),
-                  controller: eventLocation,
                 ),
+              ),
+              TextButton(
+                onPressed: () {
+                  _takeScreenshot();
+                },
+                child: Text("Share Invitation card"),
               ),
             ])));
   }
